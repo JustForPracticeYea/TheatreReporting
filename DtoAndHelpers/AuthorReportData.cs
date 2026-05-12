64ce7a5a0217eba40aa32bb2d67d9833f10a6cac
JustForPracticeYea <mumzhaalex@gmail.com>
2026-05-12 11:48:55 +0300

    Создание отчетов по спектаклям

---
+++ DtoAndHelpers/AuthorReportData.cs
@@ -0,0 +1,8 @@
+﻿namespace TheatreReportingApp.DtoAndHelpers
+{
+    public class AuthorReportData
+    {
+        public string Author { get; set; } = "";
+        public decimal TotalRoyalty { get; set; }
+    }
+}
---
+++ DtoAndHelpers/PlayReportData.cs
@@ -0,0 +1,35 @@
+﻿namespace TheatreReportingApp.DtoAndHelpers
+{
+    public class PlayReportData
+    {
+        public string PlayTitle { get; set; } = "";
+        public int PerformanceCount { get; set; }
+        public int TotalAudience { get; set; }
+        public int GrossRevenue { get; set; }
+        public int RealRevenue { get; set; }
+        public int Transferred { get; set; }
+    }
+
+    public class PlayReportTotal
+    {
+        public int TotalPerformances { get; set; }
+        public int TotalAudience { get; set; }
+        public int GrossRevenue { get; set; }
+        public int RealRevenue { get; set; }
+        public int TotalTransferred { get; set; }
+        public int TotalFreeAudience { get; set; }
+        public List<FreeCategoryData> FreeAudienceByCategory { get; set; } = new();
+    }
+
+    public class FreeCategoryData
+    {
+        public string Category { get; set; } = "";
+        public int Count { get; set; }
+    }
+
+    public class PlayReportDataContainer
+    {
+        public PlayReportTotal TotalData { get; set; } = new();
+        public List<PlayReportData> Plays { get; set; } = new();
+    }
+}
