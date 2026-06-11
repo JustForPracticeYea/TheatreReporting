e6d2db3a6b57c3a76cc2ea8f667110dbdffbc76a
JustForPracticeYea <mumzhaalex@gmail.com>
2026-06-11 11:22:05 +0300

    Добавление отчётов по авторам

--- DtoAndHelpers/AuthorReportData.cs
+++ DtoAndHelpers/AuthorReportData.cs
@@ -2,7 +2,24 @@
 {
     public class AuthorReportData
     {
-        public string Author { get; set; } = "";
-        public decimal TotalRoyalty { get; set; }
+        public int PerformanceCount { get; set; }
+        public string DatesFormatted { get; set; } = "";
+        public string PlayTitle { get; set; } = "";
+        public string ContractNumbers { get; set; } = "";      
+        public string RoyaltyPercentages { get; set; } = "";   
+        public string AuthorFees { get; set; } = "";           
+        public string AuthorJobs { get; set; } = "";          
+        public string AuthorNames { get; set; } = "";        
+        public decimal GrossRevenue { get; set; }
+        public decimal AuthorFee { get; set; }
+    }
+
+    public class AuthorReportContainer
+    {
+        public DateTime PeriodFrom { get; set; }
+        public DateTime PeriodTo { get; set; }
+        public List<AuthorReportData> Rows { get; set; } = new();
+        public decimal TotalGrossRevenue { get; set; }
+        public decimal TotalAuthorFee { get; set; }
     }
 }
